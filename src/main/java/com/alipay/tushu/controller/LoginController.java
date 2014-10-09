f361435edcbfb54ed2706c92ff0837eadad7688f
明何 <webothh@126.com>
2014-10-09 23:02:25 +0800

    添加登陆控制器

---
+++ src/main/java/com/alipay/tushu/controller/LoginController.java
@@ -0,0 +1,57 @@
+/**   
+* @Project : libernary 
+* @Package : com.alipay.tushu.controller  
+* @date 2014年10月9日 下午10:37:20 
+* Copyright (c) 2014, uestc All Rights Reserved. version V1.0 
+*/
+package com.alipay.tushu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.alipay.tushu.utils.UriContext;
+
+/** 
+ * 
+ * 
+ * @author yuanchen 2014年10月9日 下午10:37:20 
+ * @version  
+ */
+@Controller
+public class LoginController {
+	
+	/** 
+	* @Fields LOGIN_PAGE : 登陆页面
+	*/ 
+	private final static String LOGIN_PAGE = "login.vm";
+	
+	/** 
+	* @Fields SIGUP_PAGE : 注册页面
+	*/ 
+	private final static String SIGUP_PAGE = "sigup.vm";
+	
+	/** 
+	* @Description: 登陆页面
+	* 
+	* @return    
+	* @throws 
+	*/
+	@RequestMapping(value = "/login.htm", method = RequestMethod.GET)
+	public String login() {
+		return null;
+	}
+	
+	/** 
+	* @Description: 注册页面
+	* 
+	* @return    
+	* @throws 
+	*/
+	@RequestMapping(value = "/sigup.htm", method = RequestMethod.GET)
+	public String sigup(ModelMap model) {
+		model.addAttribute("UriContext", new UriContext());
+		return SIGUP_PAGE;
+	}
+}
---
+++ src/main/java/com/alipay/tushu/utils/UriContext.java
@@ -0,0 +1,20 @@
+/**   
+* @Project : libernary 
+* @Package : com.alipay.tushu.utils  
+* @date 2014年10月9日 下午10:44:20 
+* Copyright (c) 2014, uestc All Rights Reserved. version V1.0 
+*/
+package com.alipay.tushu.utils;
+
+/** 
+ * 网址上下文
+ * 
+ * @author yuanchen 2014年10月9日 下午10:44:20 
+ * @version  
+ */
+public class UriContext {
+	/** 
+	* @Fields HOST_PAGE : 主页
+	*/ 
+	public String HOST_PAGE = "http://localost:8080";
+}
